011252fa11357ae2cb77944284324abde9b11911
trba1810 <trba8l@live.com>
2024-02-04 23:03:59 +0100

    add signalr package and make hub for chatting

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReciveMessage", "admin", $"{conn.Username} has joined");
+        }
+    }
+}
