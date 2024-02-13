0871e41f1c4ac70378bc989f7d8f7d7493943bd3
trba1810 <trba8l@live.com>
2024-02-13 01:16:45 +0100

    finish chatroom ui

--- ChatApp/ChatRoomFront/src/App.jsx
+++ ChatApp/ChatRoomFront/src/App.jsx
@@ -1,10 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Container, Col } from "react-bootstrap";
 import "./App.css";
+import WaitingRoom from "./components/waitingroom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <Container>
+          <Row className="px-5 my-5">
+            <Col sm="12">
+              <h1 className="font-weight-light">Welcome to Chat App</h1>
+            </Col>
+          </Row>
+          <WaitingRoom />
+        </Container>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ ChatApp/ChatRoomFront/src/components/waitingroom.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Row, Button, Form, Col } from "react-bootstrap";
+
+const WaitingRoom = ({ joinChatRoom }) => {
+  const [username, setUsername] = useState();
+  const [chatroom, setChatRoom] = useState();
+
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        joinChatRoom(username, chatroom);
+      }}
+    >
+      <Row className="px-5 py-5">
+        <Col sm={12}>
+          <Form.Group>
+            <Form.Control
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Form.Control
+              placeholder="ChatRoom"
+              onChange={(e) => setChatRoom(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+        <Col sm={12}>
+          <hr />
+          <Button variant="success" type="submit">
+            Join
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default WaitingRoom;
