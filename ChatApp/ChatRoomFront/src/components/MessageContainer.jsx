089aeffc5981ae7600a81499a986227328fe5a96
trba1810 <trba8l@live.com>
2024-03-05 19:29:00 +0100

    finish cleaning code

--- ChatApp/ChatRoomFront/src/components/MessageContainer.jsx
+++ ChatApp/ChatRoomFront/src/components/MessageContainer.jsx
@@ -1,23 +1,5 @@
 import Table from "react-bootstrap/Table";
 
-// const MessageContainer = ({ messages }) => {
-//   return (
-//     <div>
-//       {messages.map((message, index) => (
-//         <Table striped bordered key={index}>
-//           <tbody>
-//             <tr>
-//               <td>{message.msg}</td>
-//             </tr>
-//           </tbody>
-//         </Table>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default MessageContainer;
-
 const MessageContainer = ({ messages }) => {
   return (
     <div>
